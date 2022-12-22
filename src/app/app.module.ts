a4547618a525592abe1f7b6dec92000a033b20ca
Benjamin Klein <benjamink.lein@hotmail.com>
2022-12-22 16:10:03 +0100

    Add color depending on log type (warning, error) and make text shorter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { DecreaseTextPipe } from './core/pipes/decrease-text.pipe';
+import { StatusToColorPipe } from './core/pipes/status-to-color.pipe';
 import { LogDetailedComponent } from './log-list/log-detailed/log-detailed.component';
 import { LogListComponent } from './log-list/log-list.component';
 
@@ -8,7 +10,9 @@ import { LogListComponent } from './log-list/log-list.component';
   declarations: [
     AppComponent,
     LogListComponent,
-    LogDetailedComponent
+    LogDetailedComponent,
+    StatusToColorPipe,
+    DecreaseTextPipe
   ],
   imports: [
     BrowserModule
---
+++ src/app/core/pipes/decrease-text.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'decreaseText'})
+export class DecreaseTextPipe implements PipeTransform {
+    transform(value: string, maxLength: number) {
+        let shortend = value;
+        if(value.length >= maxLength) {
+            shortend = value.substring(0, maxLength) + ".";
+        }
+
+        return shortend.replace(/^(Log)/,"");
+    }
+}
